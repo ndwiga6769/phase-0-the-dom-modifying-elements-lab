37c3db81038b56d2c2e3dfa24aa8ceff8e555562
danwycliff <danwycliff6769@gmail.com>
2023-07-18 08:40:54 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Danwycliff is the champion"
